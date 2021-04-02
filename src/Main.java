3d7c3b98d20a70d6b5a67123e1aee27fff78e627
Binura Nazarbekova <11k.binurae@gmail.com>
2021-04-02 15:11:58 +0600

    Readme.md mod

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "379098104456279";
+        String number = "3589023955948788549";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
